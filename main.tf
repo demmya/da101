c552da2b9b8811903d26ee20a03cde1709e77e0b
Demmy Adeyemo <remedyman15@gmail.com>
2019-10-27 15:17:48 +0000

    Jenkins mainSG update

--- main.tf
+++ main.tf
@@ -39,8 +39,8 @@ resource "aws_security_group" "appserver_allow" {
   }
 
 ingress {
-    from_port   = 8081
-    to_port     = 8080
+    from_port   = 8082
+    to_port     = 8082
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
