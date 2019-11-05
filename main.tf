be83b71f30b82bcdd4c8773740e29a99523b2589
Demmy Adeyemo <remedyman15@gmail.com>
2019-11-05 19:16:35 +0000

    updated main

--- main.tf
+++ main.tf
@@ -11,12 +11,16 @@ resource "aws_instance" "AppServerBox" {
   user_data                   = "${data.template_file.appserver-data.rendered}"
   #subnet_id                   = "${aws_subnet.public[0]}"
   subnet_id              = "${var.subnet_id}"
-  # vpc_security_group_ids = ["${aws_security_group.appserver_allow.id}"]
-  security_group_id = "${var.security_group_id}"
+  vpc_security_group_ids = "${var.security_group_id}"
 
   tags = {
     Name = "AppServerBox"
   }
 }
- 
+
+resource "aws_security_group" "appserver_allow" {
+  security_group_id  = "${var.security_group_id}"
+  description = "Allow all inbound traffic"
+  vpc_id      = "${var.vpc_id}"
+}
 
