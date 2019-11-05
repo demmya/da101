8c1367580b5b57ea90db3b2484a48fe505aa0e8f
Demmy Adeyemo <remedyman15@gmail.com>
2019-11-05 19:47:12 +0000

    updated main

--- main.tf
+++ main.tf
@@ -9,23 +9,17 @@ resource "aws_instance" "AppServerBox" {
   ebs_optimized               = false
   key_name                    = "${var.key_name}"
   user_data                   = "${data.template_file.appserver-data.rendered}"
-  #subnet_id                   = "${aws_subnet.public[0]}"
-  subnet_id              = "${var.subnet_id}"
-  vpc_security_group_ids = "sg-05ef7fe37202b8188"
+  #subnet_id                  = "${aws_subnet.public[0]}"
+  subnet_id                    = "${var.subnet_id}"
+  vpc_security_group_ids = ["${aws_security_group.appserver_allow.id}"]
 
   tags = {
     Name = "AppServerBox"
   }
 }
 
-#resource "aws_security_group" "appserver_allow" {
- # security_group_id  = "${var.security_group_id}"
-  #description = "Allow all inbound traffic"
-  #vpc_id      = "${var.vpc_id}"
-
-  
-  #tags = {
-  #  Name = "Appserver-SG"
- # }
-#}
+resource "aws_security_group" "appserver_allow" {
+  vpc_id      = "${var.vpc_id}"
+  security_group_id = "sg-05ef7fe37202b8188"
+}
 
